4b17f5387725d94257674ad9df4061bf265f5caa
paulathejennifer <paulathejennifer@gmail.com>
2025-05-28 22:00:26 +0300

    async, await and setTimeout functions

---
+++ index.js
@@ -0,0 +1,67 @@
+//Question 1: Email reminder
+//pseudocode
+//1. Define a function; wait(milliseconds) 
+//2. Return a Promise that resolves after 'milliseconds' time that is specified
+//3. Define async function sendReminder which takes email as ParameterDecorator
+//4. the async function waits for five seconds(5000 ms)
+//5. It then prints a reminder: "Reminder sent to [email]"
+//6. Call the sendReminder for 3 different emails
+
+
+function wait(ms){
+    return new Promise(resolve=>setTimeout(resolve, ms));
+}
+
+async function sendReminder(email){
+    await wait(5000);
+    console.log(`Reminder sent to ${email}`);
+    
+}
+
+sendReminder('paula@gmail.com')
+sendReminder('jane@gmail.com')
+sendReminder('kangethe@gmail.com')
+
+
+//Question 2: Login attempts with delay after 1 second
+//pseudocode
+//1. Define function attemptLogin which takes in attemptNumber as the parameter
+//2. After one second, if attemptNumber is 3: resolve promise with 'Login successful'
+//3. Else reject promise with 'Login failed'
+//4. Define async function tryLogin for attempts numbers from 1 to 3
+//5. Try to call the attemptLogin
+//6. If it is successful print 'Login successful' and stop further attempts
+//7. if failed print attempt {attemptNumber} failed
+//8. esle if it failed after 3 attempt print  'Login failed after 3 attempts'
+
+
+function attemptLogin(attempt){
+    return new Promise((resolve, reject)=> setTimeout(()=>{
+        if (attempt ===3 ){
+            resolve('Login successful')
+        }
+        else{
+            reject('Login failed'), 1000
+        };
+    })
+)
+}
+
+async function tryLogin() {
+    for (let i = 1; i <= 3;i++){
+        try{
+            const result = await attemptLogin(i)
+            console.log(result);
+            return;
+        }
+        catch(error){
+            console.log(`Attempt : ${i} ${error}`);
+            console.log(i);
+            
+            
+        }
+    }
+    console.log("Login failed after 3 attempts");
+    
+}
+tryLogin()
